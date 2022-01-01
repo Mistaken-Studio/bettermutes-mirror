d3d71e711bec7e81d315e630c68052f2b569964c
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-01-01 20:35:08 +0100

    change message

--- BetterMutes/Handler.cs
+++ BetterMutes/Handler.cs
@@ -43,7 +43,7 @@ namespace Mistaken.BetterMutes
         {
             while (true)
             {
-                // Normaly I whould use MuteHandler.Reload() but it console logs "Loading saved mutes..." and I don't what it
+                // Normaly I whould use MuteHandler.Reload() but it generate console logs "Loading saved mutes..." and I don't what it
                 global::MuteHandler._path = GameCore.ConfigSharing.Paths[1] + "mutes.txt";
                 try
                 {
@@ -92,18 +92,26 @@ namespace Mistaken.BetterMutes
         {
             if (!PluginHandler.Instance.Config.KickMuted)
                 return;
+
             var mute = MuteHandler.GetMute(ev.UserId);
+
             if (!mute.HasValue)
                 return;
+
             if (mute.Value.Intercom)
                 return;
+
             var writer = new LiteNetLib.Utils.NetDataWriter();
             writer.Put((byte)10);
             string reason = mute.Value.Reason == "removeme" ? "No reason provided" : mute.Value.Reason;
             if (mute.Value.EndTime == -1)
-                writer.Put($"You are muted so you can't play on RolePlay servers.. You are muted for \"{reason}\", mute has no end date, ask Admin to unmute you.");
+
+                // writer.Put($"You are muted so you can't play on RolePlay servers.. You are muted for \"{reason}\", mute has no end date, ask Admin to unmute you.");
+                writer.Put($"You are muted and this server is not allowing muted players to join.. You are muted for \"{reason}\", mute has no end date, ask Admin to unmute you.");
             else
-                writer.Put($"You are muted so you can't play on RolePlay servers.. You are muted for \"{reason}\" until {new DateTime(mute.Value.EndTime):dd.MM.yyyy HH:mm:ss} UTC");
+
+                // writer.Put($"You are muted so you can't play on RolePlay servers.. You are muted for \"{reason}\" until {new DateTime(mute.Value.EndTime):dd.MM.yyyy HH:mm:ss} UTC");
+                writer.Put($"You are muted and this server is not allowing muted players to join.. You are muted for \"{reason}\" until {new DateTime(mute.Value.EndTime):dd.MM.yyyy HH:mm:ss} UTC");
             ev.Request.Reject(writer);
             ev.Disallow();
         }
